05e47c4d887c126602ae52c4f7dbf890b4c1f84b
Mark Greer <mgreer@animalcreek.com>
2017-01-05 12:42:04 -0700

    doxygen: Add doxygen html files, et. al.
    Add the doxygen html files, et. al., that are produced by running
    
    	$ doxygen docs/Doxyfile
    
    All of the files are under the 'docs' directory and can be viewed
    from the following URL:
    
    	https://animalcreek.github.io/libi2c
    
    Signed-off-by: Mark Greer <mgreer@animalcreek.com>

---
+++ docs/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['libi2c_20_2d_20i2c_20helper_20library',['libi2c - I2C Helper Library',['../index.html',1,'']]]
+];
---
+++ docs/search/functions_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['i2c_5fdestroy',['i2c_destroy',['../i2c_8h.html#a8ef17d513f927814c3372efe5805a26a',1,'i2c_destroy(void *i2c_handle):&#160;i2c.c'],['../i2c_8c.html#a8ef17d513f927814c3372efe5805a26a',1,'i2c_destroy(void *i2c_handle):&#160;i2c.c']]],
+  ['i2c_5finit',['i2c_init',['../i2c_8h.html#a4aa26625ee83decb06c0d114dd972656',1,'i2c_init(uint8_t i2c_id, uint16_t i2c_addr, void **i2c_handlep):&#160;i2c.c'],['../i2c_8c.html#a4aa26625ee83decb06c0d114dd972656',1,'i2c_init(uint8_t i2c_id, uint16_t i2c_addr, void **i2c_handlep):&#160;i2c.c']]],
+  ['i2c_5fread',['i2c_read',['../i2c_8h.html#a3a833ed235f1e3ca8dfb9ba3e1a1d077',1,'i2c_read(void *i2c_handle, void *offset, size_t offset_len, void *buf, size_t buf_len):&#160;i2c.c'],['../i2c_8c.html#a3a833ed235f1e3ca8dfb9ba3e1a1d077',1,'i2c_read(void *i2c_handle, void *offset, size_t offset_len, void *buf, size_t buf_len):&#160;i2c.c']]],
+  ['i2c_5fwrite',['i2c_write',['../i2c_8h.html#a6cb466ae81b3f3b25c653ec6d51b3150',1,'i2c_write(void *i2c_handle, void *offset, size_t offset_len, void *buf, size_t buf_len):&#160;i2c.c'],['../i2c_8c.html#a6cb466ae81b3f3b25c653ec6d51b3150',1,'i2c_write(void *i2c_handle, void *offset, size_t offset_len, void *buf, size_t buf_len):&#160;i2c.c']]]
+];
